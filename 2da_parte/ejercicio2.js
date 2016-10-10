4013d7db9742864aacab201048f3096530e0c881
MonicaGalicia <monica.garmz@gmail.com>
2016-10-10 21:25:11 +0000

    segunda parte

---
+++ 2da_parte/ejercicio2.js
@@ -0,0 +1,18 @@
+var persona{
+	this.nombre:"nombre",
+	this.edad:25,
+	this.género:"Masculino",
+	this.obtDetalles=Function(){
+
+	} var estudiante{
+	   	this.curso:"curso",
+	    this.grupo:"grupo",
+	    this.registrar=Function(){
+		
+	} var profesor{
+	   	this.asignatura:"asignatura",
+	    this.nivel:"nivel",
+	    this.asignar=Function(){
+		
+	}  
+}
